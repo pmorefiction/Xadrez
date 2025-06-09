425f8530f6d779630245c266982ba8df0004ef95
Samara Chaves <samarachaves1999@hotmail.com>
2025-06-09 19:38:01 -0300

    Create Xadrez.c
    Adicionei o movimento da Torre, Bispo e Rainha

---
+++ Xadrez.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    printf("Movimento da Torre (5 casas para a Direita):\n");
+    for (int i = 1; i <= 5; i++) {
+        printf("Direita\n");
+    }
+
+    printf("\nMovimento do Bispo (5 casas na Diagonal Cima + Direita):\n");
+    int j = 1;
+    while (j <= 5) {
+        printf("Cima Direita\n");
+        j++;
+    }
+
+    printf("\nMovimento da Rainha (8 casas para a Esquerda):\n");
+    int k = 1;
+    do {
+        printf("Esquerda\n");
+        k++;
+    } while (k <= 8);
